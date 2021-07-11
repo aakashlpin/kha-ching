8ec89a73c2985ba6d4459ef921688df67edd926e
Aakash Goel <aakash.lpin@gmail.com>
2021-07-11 14:45:36 +0530

    curl: don't fail it

--- bootup.sh
+++ bootup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -xe
 sleep 5
-curl --output /dev/null --silent --head --fail http://localhost:${PORT:-8080}/api/user
+curl -L -sS http://localhost:${PORT:-8080}/api/user
 exit 0
