db855f4cb1924532a1c63f44c5a4ae7dd382cd7a
Aakash Goel <aakash.lpin@gmail.com>
2021-07-11 14:28:58 +0530

    ft: fallback to user default PORT

--- bootup.sh
+++ bootup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -xe
-sleep 10
-curl --output /dev/null --silent --head --fail http://localhost:8080/api/user
+sleep 5
+curl --output /dev/null --silent --head --fail http://localhost:${PORT:-8080}/api/user
 exit 0
