b67f1305db9fe416063bd7640f9466c9406d0e3f
Aakash Goel <aakash.lpin@gmail.com>
2021-07-11 14:19:06 +0530

    hf: bootup with localhost

--- bootup.sh
+++ bootup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -xe
 sleep 10
-curl --output /dev/null --silent --head --fail $NEXT_PUBLIC_APP_URL/api/user
+curl --output /dev/null --silent --head --fail http://localhost:8080/api/user
 exit 0
