b638ccd64c6236b3467b93e4701ce6236b946e19
Aakash Goel <aakash.lpin@gmail.com>
2021-07-22 21:04:28 +0530

    [tested] handle NetworkException on placeOrder and find an existing order in broker system

--- __tests__/unit/orderRetry.test.js
+++ __tests__/unit/orderRetry.test.js
@@ -95,3 +95,56 @@ test('should return false when order history api check times out', async () => {
 
   expect(ensured).toBe(false)
 })
+
+test('should handle `placeOrder` NetworkException and then find an existing completed order in broker system', async () => {
+  let kite = syncGetKiteInstance(user)
+  kite = {
+    ...kite,
+    placeOrder: jest.fn().mockRejectedValue({
+      status: 'error',
+      error_type: 'NetworkException'
+    }),
+    getOrders: jest.fn(() => Promise.resolve([{
+      order_id: '210722200262556',
+      status: 'COMPLETE',
+      variety: 'regular',
+      exchange: 'NFO',
+      tradingsymbol: 'BANKNIFTY2172234500PE',
+      order_type: 'MARKET',
+      transaction_type: 'SELL',
+      validity: 'DAY',
+      product: 'MIS',
+      quantity: 250,
+      tag: 'X0uE0cKR'
+    }])).mockImplementationOnce(() => Promise.reject(new Error({
+      status: 'error',
+      error_type: 'NetworkException'
+    }))),
+    getOrderHistory: jest.fn().mockImplementation(() => Promise.resolve([{
+      status: 'COMPLETE'
+    }]))
+  }
+
+  expect(kite).toBeDefined()
+
+  const ensured = await remoteOrderSuccessEnsurer({
+    __kite: kite,
+    orderProps: {
+      orderTag: 'X0uE0cKR',
+      tradingsymbol: 'BANKNIFTY2172234500PE',
+      quantity: 250,
+      product: 'MIS',
+      transaction_type: 'SELL',
+      exchange: 'NFO'
+    },
+    ensureOrderState: kite.STATUS_COMPLETE,
+    retryEveryMs: ms(1),
+    retryAttempts: 2,
+    orderStatusCheckTimeout: ms(5),
+    user
+  })
+
+  console.log({ ensured })
+
+  expect(ensured).toBe(true)
+})
