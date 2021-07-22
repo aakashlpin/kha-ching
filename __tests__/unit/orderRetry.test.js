c5060bcf658a9ad600db5545275d54425635f515
Aakash Goel <aakash.lpin@gmail.com>
2021-07-22 16:18:29 +0530

    [tested] withRemoteRetry

---
+++ __tests__/unit/orderRetry.test.js
@@ -0,0 +1,22 @@
+import { ms, withRemoteRetry } from '../../lib/utils'
+
+test('should retry failed remote response', async () => {
+  const remoteFn = jest.fn()
+    .mockResolvedValue(true)
+    .mockRejectedValueOnce(new Error('Async error'))
+
+  const res = await withRemoteRetry(remoteFn, ms(5))
+  console.log({ res })
+  expect(res).toEqual(true)
+})
+
+test('should fail after 2 seconds', async () => {
+  const remoteFn = jest.fn()
+    .mockResolvedValue(true)
+    .mockRejectedValueOnce(new Error('Async error'))
+    .mockRejectedValueOnce(new Error('Async error'))
+    .mockRejectedValueOnce(new Error('Async error'))
+
+  const res = await withRemoteRetry(remoteFn, ms(2))
+  expect(res).toEqual(null)
+})
