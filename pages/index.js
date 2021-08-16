e3e24f6ca03f2d842eac3964a9b97a17ea3484dc
Aakash Goel <aakash.lpin@gmail.com>
2021-08-16 23:15:26 +0530

    fx: redirect to dashboard if logged in

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import useUser from '../lib/useUser'
 import styles from '../styles/Home.module.css'
 
 export default function Home () {
+  useUser({ redirectTo: '/dashboard', redirectIfFound: true })
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
