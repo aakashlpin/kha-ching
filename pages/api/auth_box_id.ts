a1985c207d9a144ce408dc9c67ec784a66547fa9
Aakash Goel <aakash.lpin@gmail.com>
2021-09-26 19:20:21 +0530

    ft: Added subscription notice to dashboard

--- pages/api/auth_box_id.ts
+++ pages/api/auth_box_id.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
 import dayjs from 'dayjs'
 import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { SUBSCRIPTION_TYPE } from '../../lib/constants'
 dayjs.extend(isSameOrBefore)
 
-const authBoxId = async (req, res) => {
+const authBoxId = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+): Promise<any> => {
   try {
     const { data } = await axios(
       `https://api.airtable.com/v0/${
@@ -21,6 +26,7 @@ const authBoxId = async (req, res) => {
     const { records = [] } = data
     if (!records.length) {
       return res.json({
+        type: SUBSCRIPTION_TYPE.NOT_SUBSCRIBER,
         allowed: false,
         message: 'User not found.'
       })
@@ -31,13 +37,19 @@ const authBoxId = async (req, res) => {
       fields: { Expires }
     } = user
 
+    const isPremiumUser = user.fields['Paid Subscriber']
+    const isClubUser = user.fields['Xtra Subscriber']
+
     return res.json({
+      type: SUBSCRIPTION_TYPE.SUBSCRIBER,
       expireOn: Expires,
-      allowed: dayjs().isSameOrBefore(dayjs(Expires, 'YYYY-MM-DD'), 'day')
+      allowed: dayjs().isSameOrBefore(dayjs(Expires, 'YYYY-MM-DD'), 'day'),
+      isPremiumUser,
+      isClubUser
     })
   } catch (e) {
     console.log('[auth_box_id] error', e)
-    return res.json({
+    return res.status(500).json({
       allowed: false,
       message: 'Remote error'
     })
