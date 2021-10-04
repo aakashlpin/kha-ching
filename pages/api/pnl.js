e1cbea3e7825c139d14f7745ecc9280f41a5c158
anil1kuppa <anil.kuppa@gmail.com>
2021-10-04 23:26:14 +0530

    Correcting the profit

--- pages/api/pnl.js
+++ pages/api/pnl.js
@@ -24,10 +24,10 @@ export default withSession(async (req, res) => {
     const { data: orders } = await axios(
       `${process.env.DATABASE_HOST_URL}/odr_${process.env.DATABASE_USER_KEY}/${orderTag}`
     )
-    const {orclProfit}=await axios (
+    const {data:{profit}}=await axios (
      `${process.env.ORCL_HOST_URL}/profits/${orderTag}`
     )
-    console.log(`Profit from ORCL is:${orclProfit.profit} `)
+    console.log(`Profit from ORCL is:${profit} `)
 
     if (!orders.length) {
       return res.json({ error: 'PnL not ready yet!' })
