0333e73fd67c87d8a992b28e62101c80e11e6068
Aakash Goel <aakash.lpin@gmail.com>
2021-10-23 12:27:20 +0530

    ft: enable admin work auth

--- lib/session.ts
+++ lib/session.ts
@@ -6,33 +6,64 @@ import { KiteConnect } from 'kiteconnect'
 import './queue-processor'
 import './exit-strategies'
 import './watchers'
-import { setUserSession } from './utils'
+import { setUserSession, premiumAuthCheck } from './utils'
+import { KiteProfile } from '../types/kite'
 
-const withAdminCheck = (handler) => {
-  return async function withAdminWrapper(req, res) {
-    const kiteKey = req.headers['signalx-kite-key'];
-    const kiteToken = req.headers['signalx-kite-token'];
-    if (kiteKey && kiteToken) {
-      console.log('key and token found in headers. ateempting to connect kite and save session')
+const withAdminCheck = handler => {
+  return async function withAdminWrapper (req, res) {
+    const sxApiKey = req.headers['signalx-api-key']
+    const kiteApiKey = req.headers['signalx-kite-key']
+    const kiteAccessToken = req.headers['signalx-kite-token']
+
+    if (!sxApiKey && !kiteApiKey) {
+      // UI based flow
+      return handler(req, res)
+    }
+
+    // for premium and club users
+    let validSxUser = false
+    try {
+      validSxUser = await premiumAuthCheck()
+    } catch (e) {
+      validSxUser = false
+    }
+
+    // block intrusion
+    if (!validSxUser) {
+      return handler(req, res)
+    }
+
+    if (!kiteAccessToken) {
+      // sx admin work, no broker authorization
+      await setUserSession(req, {} as KiteProfile)
+    } else {
+      console.log(
+        'key and token found in headers. attempting to connect kite and save session'
+      )
       try {
         const kc = new KiteConnect({
-          api_key: kiteKey,
-          access_token: kiteToken
-        });
+          api_key: kiteApiKey,
+          access_token: kiteAccessToken
+        })
 
-        const kiteProfile = await kc.getProfile();
-        await setUserSession(req, { access_token: kiteToken, ...kiteProfile });
-        console.log('session generated')
+        const kiteProfile = await kc.getProfile()
+        await setUserSession(req, {
+          access_token: kiteAccessToken,
+          ...kiteProfile
+        })
+        console.log('session generated from headers')
+        return handler(req, res)
       } catch (error) {
         console.log(error)
-        return res.status(403).send('Forbidden. Unauthorized kry or token provided')
+        return res
+          .status(403)
+          .send('Forbidden. Unauthorized key or token provided')
       }
     }
-    return handler(req, res)
   }
 }
 
-export default function withSession(handler) {
+export default function withSession (handler) {
   return withIronSession(withAdminCheck(handler), {
     password: process.env.SECRET_COOKIE_PASSWORD!,
     cookieName: 'khaching/kite/session',
