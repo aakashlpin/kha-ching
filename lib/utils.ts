e4fe4b326d9b188a00646a09c252be46b71229dc
anil1kuppa <anil.kuppa@gmail.com>
2021-12-23 00:19:22 +0530

    Correcting some Lint Rules

--- lib/utils.ts
+++ lib/utils.ts
@@ -867,7 +867,7 @@ export async function premiumAuthCheck (): Promise<any> {
   )
 }
 
-export const orclsodaUrl: string = `${ORCL_HOST_URL}/soda/latest`
+export const orclsodaUrl = `${ORCL_HOST_URL}/soda/latest`
 
 export const isMockOrder = () =>
   process.env.MOCK_ORDERS ? JSON.parse(process.env.MOCK_ORDERS) : false
