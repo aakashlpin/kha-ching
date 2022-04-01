527f7bfdac6b29f5e4522da0ac239f2bdfa316f6
Aakash Goel <aakash.lpin@gmail.com>
2022-04-01 15:05:27 +0530

    updated freeze qty

--- lib/constants.ts
+++ lib/constants.ts
@@ -57,8 +57,7 @@ export const INSTRUMENT_DETAILS: Record<INSTRUMENTS, INSTRUMENT_PROPERTIES> = {
     exchange: 'NSE',
     strikeStepSize: 100,
     // [27501-40000]
-    // freezeQty: 100
-    freezeQty: 75
+    freezeQty: 1200
   },
   [INSTRUMENTS.FINNIFTY]: {
     lotSize: 40,
