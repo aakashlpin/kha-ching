f11b76a390090dc9af729eee3dfce4be2741f21c
Aakash Goel <aakash.lpin@gmail.com>
2022-12-16 18:57:43 +0530

    fx: use signalx_api_key for tokens

--- pages/api/refresh_token.ts
+++ pages/api/refresh_token.ts
@@ -7,9 +7,10 @@ import { storeAccessTokenRemotely } from '../../lib/utils';
 const DATABASE_HOST_URL = process.env.DATABASE_HOST_URL;
 const KITE_API_SECRET = process.env.KITE_API_SECRET;
 const KITE_API_KEY = process.env.KITE_API_KEY;
+const SIGNALX_API_KEY = process.env.SIGNALX_API_KEY;
 
 const runner = async (req, res) => {
-  const storedTokens = await axios(`${DATABASE_HOST_URL}/pvt_${KITE_API_KEY}/tokens`)
+  const storedTokens = await axios(`${DATABASE_HOST_URL}/pvt_${SIGNALX_API_KEY}/tokens`)
   const { data } = storedTokens
   const [ latestRecord ] = data
   const { access_token, refresh_token } = latestRecord
@@ -22,8 +23,7 @@ const runner = async (req, res) => {
   try {
     // see if we're able to fetch profile with the access token
     // in case access token is expired, then log out the user
-    // await kc.getProfile()
-    throw 'error'
+    await kc.getProfile()
   } catch (e) {
     console.log('access token expired')
     const responseOnRenew = await kc.renewAccessToken(refresh_token, KITE_API_SECRET)
